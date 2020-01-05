f006801565e2078eec6eb5fcb2d0a4336fc5d0c1
Oana Plesu <plesu.oana@gmail.com>
2020-01-05 15:17:55 +0200

    add initial spring project

---
+++ librarymanager/src/main/java/com/librarymanager/LibrarymanagerApplication.java
@@ -0,0 +1,13 @@
+package com.librarymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarymanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarymanagerApplication.class, args);
+	}
+
+}
